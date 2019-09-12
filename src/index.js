56e3e071fa543ec35fdef2002ad94273aa1f3e0e
unknown <1628377463@qq.com>
2019-09-12 11:35:02 +0800

    routerinit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import RootRouter from './router';
 import * as serviceWorker from './serviceWorker';
-import 'antd/dist/antd.css'; //引入antd全局样式
+
+// import 'antd/dist/antd.css'; //引入antd全局样式
 import axios from "utils/axios.js"
+
 React.Component.prototype.$axios=axios
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<RootRouter>你好</RootRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
